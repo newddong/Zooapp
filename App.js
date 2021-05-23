eedb089a24042f053291bfc2d0b6f8c1261df856
DESKTOP-TINVNIK\lky <newddong@hanmail.net>
2021-05-24 00:48:12 +0900

    최초 커밋

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Login from './login';
+
+const App: () => Node = () => {
+  
+  
+  
+  return (
+    
+    <Login/>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  }
+});
+
+export default App;
