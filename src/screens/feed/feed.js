620d9b2a840f6d896a8877c87ed81bf4f32c24a4
DESKTOP-TINVNIK\lky <newddong@hanmail.net>
2021-06-03 18:18:43 +0900

    headermode

--- src/screens/feed/feed.js
+++ src/screens/feed/feed.js
@@ -1,21 +1,18 @@
 import React, {useState} from 'react';
 
-
 import Profile from './profile/profile';
 import AppHome from './home/apphome';
 
 import Header from '../header/header';
 
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 const Feed = () => {
-	
 	return (
-		<Stack.Navigator initialRouteName="AppHome" >
-			<Stack.Screen name="AppHome" component={AppHome} options={{header:()=><Header/>		
-		}}/>
+		<Stack.Navigator initialRouteName="AppHome" headerMode='float'> 
+			<Stack.Screen name="AppHome" component={AppHome} options={{header: () => <Header />}}/>
 			<Stack.Screen name="Profile" component={Profile} />
 		</Stack.Navigator>
 	);
