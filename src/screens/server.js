a90929e64f25b170399f37c2e799ff64f91525b1
DESKTOP-TINVNIK\lky <newddong@hanmail.net>
2021-08-05 12:34:51 +0900

    환경설정 변경

--- src/screens/server.js
+++ src/screens/server.js
@@ -1,7 +1,8 @@
 import CookieManager from '@react-native-cookies/cookies';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const serveruri = 'http://10.0.2.2:3000';
+// export const serveruri = 'http://10.0.2.2:3000';
+export const serveruri = 'https://api.zoodoongi.net';
 
 export const cookieReset = async (token) => {
 	await CookieManager.clearAll();
